bf2dcdf58f45e2584d07d145c37a582c3f790728
Akash <57212963+Akash-152000@users.noreply.github.com>
2023-04-14 14:19:09 +0530

    Created Routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./screens/Home/Home";
+import Movies from "./screens/Movies/Movies";
+import Tv from "./screens/TV/Tv";
 import TrendingState from "./context/Trending/TrendingState";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <TrendingState>
-      <div className="App">
-        <Navbar />
-        <Home />
-      </div>
+      <Router>
+        <Navbar/>
+        <div className="App">
+          <Routes>
+            <Route exact path="/" element={<Home/>}/> 
+            <Route exact path="/movies" element={<Movies/>}/> 
+            <Route exact path="/tvshows" element={<Tv/>}/> 
+          </Routes> 
+        </div>
+      </Router>
     </TrendingState>
   );
 }
