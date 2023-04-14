1f9a6703cd53feb82bb4c9ab90a53907459cf73c
Akash <57212963+Akash-152000@users.noreply.github.com>
2023-04-14 14:20:40 +0530

    Created Movies and TV screens

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,17 +3,20 @@ import React from "react";
 const Card = (props) => {
   return (
     <div>
-      <div className="card mx-2" style={{"width": "18rem"}}>
-        <img src={`https://image.tmdb.org/t/p/w500${props.poster}`} className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            {props.title}
-          </p>
+      {/* style={{"width": "18rem"}} */}
+      <div className="card mx-2" style={{ height:"30rem",width: "20rem" }}>
+        <img
+          src={`https://image.tmdb.org/t/p/w500${props.poster}`}
+          className="card-img-top"
+          alt="..."
+        />
+        {/* <div className="card-body">
+          <h5 className="card-title ">{props.title}</h5>
+          <p className="card-text"></p>
           <a href="#" className="btn btn-primary">
             Go somewhere
           </a>
-        </div>
+        </div> */}
       </div>
     </div>
   );
--- src/context/Trending/TrendingState.js
+++ src/context/Trending/TrendingState.js
@@ -1,17 +1,19 @@
 import trendingContext from "./trendinContext";
 import { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 const TrendingState = (props) => {
   const [movies, setMovies] = useState({});
   const [tvShows, setTvShows] = useState({});
   const [loading, setLoading] = useState(true);
+  const [searchItem, setSearchItem] = useState("");
+  const [searchData, setSearchData] = useState({});
+
+  const API_KEY = "c33a3ff17bd8ddd11d6b09f3caaabee8";
 
   useEffect(() => {
     axios
-      .get(
-        "https://api.themoviedb.org/3/trending/movie/day?api_key=c33a3ff17bd8ddd11d6b09f3caaabee8"
-      )
+      .get(`https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`)
       .then((response) => {
         const data = response.data;
         setMovies(data);
@@ -22,9 +24,7 @@ const TrendingState = (props) => {
       });
 
     axios
-      .get(
-        "https://api.themoviedb.org/3/trending/tv/day?api_key=c33a3ff17bd8ddd11d6b09f3caaabee8"
-      )
+      .get(`https://api.themoviedb.org/3/trending/tv/day?api_key=${API_KEY}`)
       .then((response) => {
         const data = response.data;
         setTvShows(data);
@@ -35,8 +35,32 @@ const TrendingState = (props) => {
       });
   }, []);
 
+  const search = (searchQuery) => {
+    axios
+    .get(
+      `https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&language=en-US&query=${searchQuery}`
+      )
+      .then((response) => {
+        const data = response.data;
+        setSearchData({...searchData, data});
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  };
+
   return (
-    <trendingContext.Provider value={{movies,tvShows,loading }}>
+    <trendingContext.Provider
+      value={{
+        movies,
+        tvShows,
+        loading,
+        searchItem,
+        setSearchItem,
+        search,
+        searchData,
+      }}
+    >
       {props.children}
     </trendingContext.Provider>
   );
