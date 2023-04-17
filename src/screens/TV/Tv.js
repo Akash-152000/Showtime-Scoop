e7377b0fb968f332596209f58f740624c739f9cf
Akash <57212963+Akash-152000@users.noreply.github.com>
2023-04-17 18:25:30 +0530

    Updated the component

--- src/screens/TV/Tv.js
+++ src/screens/TV/Tv.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useContext } from "react";
+import trendingContext from "../../context/Trending/trendinContext";
+import Spinner from "../../components/Spinner/Spinner";
+import "./tv.css";
+import Pagination from "@mui/material/Pagination";
+import { ThemeProvider, createTheme } from "@mui/material";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 const Tv = () => {
-  return (
-    <div>Tv</div>
-  )
-}
+  const context = useContext(trendingContext);
+  const { tvData, loading, setPage } = context;
 
-export default Tv
+  return (
+    <div className="wrapper mt-5 container" style={{ maxWidth: "100vw" }}>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className="row" style={{ marginLeft: 90 }}>
+          {tvData.map((ele) => {    
+            return (
+              <div className="card mb-3 " key={ele.id} style={{display:`${ele.poster_path===null||ele.poster_path===undefined?"none":""}`}}>
+                <img
+                  src={`https://image.tmdb.org/t/p/w500${ele.poster_path}`}
+                  className="card-img-top"
+                  alt=""
+                />
+              </div>
+            );
+          })}
+          <div className="pagination">
+            <ThemeProvider theme={darkTheme}>
+              <Pagination
+                onChange={(e) => setPage(e.target.textContent)}
+                count={500}
+                size="large"
+                hidePrevButton
+                hideNextButton
+              />
+            </ThemeProvider>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Tv;
