dceb956325042d2e2c04b96081030b4176ca23e9
Akash <57212963+Akash-152000@users.noreply.github.com>
2023-04-17 15:36:37 +0530

    Poulating the component

--- src/screens/Movies/Movies.js
+++ src/screens/Movies/Movies.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import trendingContext from '../../context/Trending/trendinContext';
+import Spinner from '../../components/Spinner/Spinner';
 
 const Movies = () => {
+  const context = useContext(trendingContext);
+  const {movieData, loading} = context;
+ 
   return (
-    <div>Movies</div>
+    <div>
+    
+      {loading?<Spinner/>
+        :
+        <div>
+          Hloo
+        </div>
+      }
+      
+    </div>
   )
 }
 
