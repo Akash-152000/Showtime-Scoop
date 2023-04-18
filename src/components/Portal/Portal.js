e8360249fbff5fe0327dbfda85938c8fdc566b54
Akash <57212963+Akash-152000@users.noreply.github.com>
2023-04-18 23:55:43 +0530

    Populated Data from api on Portal component

--- src/components/Portal/Portal.js
+++ src/components/Portal/Portal.js
@@ -1,27 +1,35 @@
-import React, {useContext, useRef} from 'react'
-import './portal.css'
-import trendingContext from '../../context/Trending/trendinContext';
-import ReactDOM from 'react-dom'
+import React, { useContext, useRef } from "react";
+import "./portal.css";
+import trendingContext from "../../context/Trending/trendinContext";
+import ReactDOM from "react-dom";
 
 const Portal = (props) => {
+  const context = useContext(trendingContext);
+  const { title, desc, releaseDate, rating, setShowPortal, poster } = context;
 
-  const context =  useContext(trendingContext)
-  const {showPortal,setShowPortal,poster} = context;
-
-  return  ReactDOM.createPortal(
+  return ReactDOM.createPortal(
     <>
-    <div className='modal-wrapper text-light' onClick={()=>setShowPortal(false)}></div>
-    <div className='portal text-white'>
-    <img 
-          src={`https://image.tmdb.org/t/p/w500${poster}`}
-          className="portal-img-top"
-          alt="..."
+      <div
+        className="modal-wrapper text-light"
+        onClick={() => setShowPortal(false)}
+      ></div>
+      <div className="portal text-white d-flex">
+          <img
+            src={`https://image.tmdb.org/t/p/w500${poster}`}
+            className="portal-img-top"
+            alt="..."
           />
-    </div>
-    </>
-    ,
-    document.getElementById("portal") 
-  )
-}
+
+        <div className="content">
+          <h3 className="title">{title}</h3>
+          <h5 className="description">{desc}</h5>
+          <h5 className="releaseDate">{releaseDate}</h5>
+          <h5 className="rating">{rating}</h5>
+        </div>
+      </div>
+    </>,
+    document.getElementById("portal")
+  );
+};
 
 export default Portal;
