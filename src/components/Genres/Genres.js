e6b9928a411f6e6d7cf3b79ad8486bfe4f9c108d
Akash <57212963+Akash-152000@users.noreply.github.com>
2023-04-18 13:13:07 +0530

    Created Genre component

---
+++ src/components/Genres/Genres.js
@@ -0,0 +1,116 @@
+import { Chip } from "@mui/material";
+import React, { useContext, useEffect } from "react";
+import trendingContext from "../../context/Trending/trendinContext";
+import Spinner from "../Spinner/Spinner";
+import { ThemeProvider, createTheme } from "@mui/material"
+import useGenre from "../../customHook/useGenre";
+import axios from 'axios'
+
+const darkTheme = createTheme({
+  palette:{
+    mode:"dark"
+  }
+})
+
+const Generes = (props) => {
+  const context = useContext(trendingContext);
+  const { genres,setGenres,setPage, selectedGenres, setSelectedGenres, loading} = context;
+  
+
+
+  const handleAdd =(ele)=>{
+    setSelectedGenres([...selectedGenres,ele]);
+    setGenres(genres.filter((g)=>g.id!==ele.id));
+    setPage(1)
+    
+  }
+
+  const handleDelete=(genre)=>{
+    setSelectedGenres(
+      selectedGenres.filter((selected) => selected.id !== genre.id)
+    );
+    setGenres([...genres, genre]);
+    setPage(1);
+  }
+
+
+const getGenres=async ()=>{
+
+  await axios
+  .get(
+    `https://api.themoviedb.org/3/genre/${props.name}/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+  )
+  .then((response) => {
+    const data = response.data;
+    setGenres(data.genres);
+    // setLoading(false)
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
+}
+
+  useEffect(()=>{
+    
+    getGenres()
+
+  },[])
+
+  return (
+    <div>
+     
+      {
+        
+        loading?
+        <div>
+          <Spinner/>
+        
+        </div>
+        :
+        <div style={{marginBottom:30}}>
+          {/* {console.log(genreForUrl)} */}
+          
+          {
+            selectedGenres.map((ele)=>{
+              return (
+                <ThemeProvider theme={darkTheme}>
+                  <Chip 
+                    label={ele.name}  
+                    style={{marginLeft:6,marginBottom:8}}
+                    clickable
+                    color="primary"
+                    key={ele.id}
+                    onDelete={()=>handleDelete(ele)}
+                  />
+                </ThemeProvider>
+                  
+              )
+            })
+          
+          }
+          { 
+          
+            genres.map((ele)=>{
+              return (
+                <ThemeProvider theme={darkTheme}>
+                  <Chip 
+                    label={ele.name}  
+                    style={{marginLeft:6,marginBottom:8}}
+                    clickable
+                    key={ele.id}
+                    onClick={()=>handleAdd(ele)}
+                  />
+                </ThemeProvider>
+                  
+              )
+            })
+          }
+          
+        </div>
+      }
+      
+    </div>
+  );
+};
+
+export default Generes;
