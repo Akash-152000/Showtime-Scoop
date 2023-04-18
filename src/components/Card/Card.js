be3e8c10672b944a203d3f097dd9f2bdeb1f1273
Akash <57212963+Akash-152000@users.noreply.github.com>
2023-04-18 23:53:31 +0530

    Added Badge commponent to display rating

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,47 +1,54 @@
 import React, { useContext, useState } from "react";
 import './card.css'
 import trendingContext from "../../context/Trending/trendinContext";
-import Portal from '../../components/Portal/Portal';
+import Badge from '@mui/material/Badge';
+
 
 const Card = (props) => {
   const context = useContext(trendingContext)
-  const {showPortal,setShowPortal, poster, setPoster} = context
-  
-
+  const {setShowPortal, setPoster, setTitle, setDesc, setReleaseDate, setRating} = context
 
 
   const handleClick =()=>{
     setShowPortal(true)
     setPoster(props.poster)
+    
+    setDesc(props.ele.overview)
+    
+    setRating(props.ele.vote_average)
+
+    if(props.name==='Movies'){
+      setTitle(props.ele.title)
+      setReleaseDate(props.ele.release_date)
+    }
+    else{
+      setTitle(props.ele.name
+        )
+      setReleaseDate(props.ele.first_air_date)
+    }
+
   }
 
   return (
-    <div>
-      {showPortal&& <Portal/>}
-      <div className="card ">
-        {props.poster?
-        <img  onClick={handleClick}
+      <Badge badgeContent={4} color="primary">
+      <div>
+      
+        <div className="card ">
+          {props.poster?
+          <img  onClick={handleClick}
           src={`https://image.tmdb.org/t/p/w500${props.poster}`}
           className="card-img-top"
           alt=""
           />
           
-        :<div>
-          
-        </div>
+          :<div>
+            
+            </div>
       }
         </div>
-        </div>
+    </div>
+      </Badge>
   );
 };
 
 export default Card;
-
-
-{/* <div className="card-body">
-          <h5 className="card-title ">{props.title}</h5>
-          <p className="card-text"></p>
-          <a href="#" className="btn btn-primary">
-          Go somewhere
-          </a>
-        </div> */}
