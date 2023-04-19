35a6482db7d6f1815fedefb64eb4762f376edcfd
Akash <57212963+Akash-152000@users.noreply.github.com>
2023-04-19 18:07:30 +0530

    added api call on handleClick to get cast

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,6 +2,7 @@ import React, { useContext, useState } from "react";
 import "./card.css";
 import trendingContext from "../../context/Trending/trendinContext";
 import Badge from "@mui/material/Badge";
+import axios from "axios";
 
 const Card = (props) => {
   const context = useContext(trendingContext);
@@ -12,11 +13,26 @@ const Card = (props) => {
     setDesc,
     setReleaseDate,
     setRating,
+    setCast
   } = context;
 
   const [invisible, setInvisible] = useState(true)
 
-  const handleClick = () => {
+  const handleClick = async () => {
+
+    await axios
+      .get(
+        `https://api.themoviedb.org/3/movie/${props.ele.id}/credits?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+      )
+      .then((response) => {
+        const data = response.data;
+        setCast(data)
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+
+
     setShowPortal(true);
     setPoster(props.poster);
 
