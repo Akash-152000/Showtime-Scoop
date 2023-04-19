98cd6b6d933c38e46181224a4742539d9f95fb5f
Akash <57212963+Akash-152000@users.noreply.github.com>
2023-04-19 18:10:01 +0530

    Added map function to iterate cast

--- src/components/Portal/Portalcomponents/PortalDesc.js
+++ src/components/Portal/Portalcomponents/PortalDesc.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useContext } from "react";
+import "./portalDesc.css";
+import trendingContext from "../../../context/Trending/trendinContext";
 
 const PortalDesc = () => {
-  return (
-    <div>PortalDesc</div>
-  )
-}
+  const context = useContext(trendingContext);
+  const { cast } = context;
 
-export default PortalDesc
+  return (
+    <div>
+      <div className="d-flex container desc-wrapper  mt-5">
+        {cast.cast.map((ele) => {
+          return (
+            <>
+              {ele.profile_path === null ? (
+                <></>
+              ) : (
+                <div key={ele.id} className="desc-card desc-item mt-1">
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500${ele.profile_path}`}
+                    className="desc-card-img-top"
+                    alt=""
+                  />
+                  <div>
+                    <div>
+                      <b>{ele.name}</b>
+                      <br></br>
+                    </div>
+                    <h7>{`(${ele.character})`}</h7>
+                  </div>
+                </div>
+              )}
+            </>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default PortalDesc;
