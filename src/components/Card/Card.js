506986a02b8f1c2027d591ab8e12db3045a055cb
Akash <57212963+Akash-152000@users.noreply.github.com>
2023-04-21 21:42:50 +0530

    Add to favourties logic

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -13,26 +13,33 @@ const Card = (props) => {
     setDesc,
     setReleaseDate,
     setRating,
-    setCast
+    setCast,
+    setFav,
+    fav,
+    setEleInfo
   } = context;
 
-  const [invisible, setInvisible] = useState(true)
- 
-  const handleClick = async () => {
+  const [invisible, setInvisible] = useState(true);
 
+  const handleClick = async () => {
     await axios
       .get(
-        `https://api.themoviedb.org/3/${props.name==="Movies"?'movie':'tv'}/${props.ele.id}/credits?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+        `https://api.themoviedb.org/3/${
+          props.name === "Movies" ? "movie" : "tv"
+        }/${props.ele.id}/credits?api_key=${
+          process.env.REACT_APP_API_KEY
+        }&language=en-US`
       )
       .then((response) => {
         const data = response.data;
-        setCast(data)
+        setCast(data);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
     setShowPortal(true);
     setPoster(props.poster);
+    setEleInfo(props.ele)
 
     setDesc(props.ele.overview);
 
@@ -47,28 +54,56 @@ const Card = (props) => {
     }
   };
 
+
+  const handleFav =(ele)=>{
+    console.log(fav.includes(ele));
+    if(fav.includes(ele)){
+      setFav(fav.filter((g)=>g.id!==ele.id));
+    }
+    else{
+      setFav([...fav,ele])
+    }
+  }
+
+
   return (
-    <div>
-      <div className="card ">
+    <div className="card">
         <Badge
           badgeContent={Math.round(props.ele.vote_average * 10) / 10}
-          color={props.ele.vote_average>=8?'success':props.ele.vote_average>=6?'warning':'error'}
+          color={
+            props.ele.vote_average >= 8
+              ? "success"
+              : props.ele.vote_average >= 6
+              ? "warning"
+              : "error"
+          }
           invisible={invisible !== props.ele.id}
         >
           {props.poster ? (
-            <img
-              onClick={handleClick}
-              onMouseEnter={() => setInvisible(props.ele.id)}
-              onMouseLeave={() => setInvisible(true)}
-              src={`https://image.tmdb.org/t/p/w500${props.poster}`}
-              className="card-img-top"
-              alt=""
-            />
+            <div className="d-flex flex-column">
+              <img
+                onClick={handleClick}
+                onMouseEnter={() => setInvisible(props.ele.id)}
+                onMouseLeave={() => setInvisible(true)}
+                src={`https://image.tmdb.org/t/p/w500${props.poster}`}
+                className="card-img-top"
+                alt=""
+              />
+              <div className="card-body text-center text-light">
+              <i className={`${fav.includes(props.ele)?"fa-solid":"fa-regular"} fa-heart fa-lg heart`} onClick={()=>handleFav(props.ele)}></i>
+                {/* <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p> */}
+                {/* <a href="#" class="btn btn-primary">
+                  Go somewhere
+                </a> */}
+              </div>
+            </div>
           ) : (
             <div></div>
           )}
         </Badge>
-      </div>
     </div>
   );
 };
