d7167e59e2ce5e314ced6c9066e470f6d0b93f62
Akash <57212963+Akash-152000@users.noreply.github.com>
2023-04-24 15:56:03 +0530

    Added a useffect hook

--- src/screens/Search/Search.js
+++ src/screens/Search/Search.js
@@ -1,13 +1,21 @@
-import React,{useContext} from 'react'
+import React,{useContext, useEffect} from 'react'
 import './search.css'
 import fetchApiDataContext from '../../context/FetchApiData/fetchApiDataContext'
 import SearchCard from '../../components/SearchCard/SearchCard'
+import favContext from '../../context/Favourites/favContext'
 
 
 const Search = () => {
     const context = useContext(fetchApiDataContext)
     const {searchMovieData, searchTvData} = context
 
+    const contextFav = useContext(favContext)
+    const {getFav} = contextFav
+
+    useEffect(()=>{
+      getFav()
+    })
+  
   return (
     <div className='text-light'>
         <h3 className='mt-5 container mb-5'>Movie Results</h3>
