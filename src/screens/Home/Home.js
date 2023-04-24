5551341b5c23ccaa1f4a05419cf78887056ea446
Akash <57212963+Akash-152000@users.noreply.github.com>
2023-04-24 15:26:37 +0530

    connecting favourite feature to database

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,14 +1,23 @@
-import React, {useContext} from 'react'
+import React, {useContext, useEffect} from 'react'
 import fetchApiDataContext from '../../context/FetchApiData/fetchApiDataContext';
 import './home.css'
 import Trending from '../../components/Trending/Trending';
 import Spinner from '../../components/Spinner/Spinner';
 import Portal from '../../components/Portal/Portal';
+import favContext from '../../context/Favourites/favContext';
 
 const Home = () => {
 
   const context =  useContext(fetchApiDataContext)
-  const {movies, tvShows, loading, showPortal,setShowPortal} = context;
+  const {movies, tvShows, loading, showPortal,favUpdated} = context;
+
+  const contextFav = useContext(favContext)
+  const {getFav} = contextFav
+
+  useEffect(()=>{
+    getFav()
+    // console.log(dummFav);
+  },[favUpdated])
 
 
   return (
