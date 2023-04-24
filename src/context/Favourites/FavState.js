fbd6cb88d1978a31bdd48b3eff136833e6dc4132
Akash <57212963+Akash-152000@users.noreply.github.com>
2023-04-24 16:58:10 +0530

    Replaced hardcoded authtokens to localstorage tokens

--- src/context/Favourites/FavState.js
+++ src/context/Favourites/FavState.js
@@ -7,37 +7,38 @@ const FavState = (props) => {
     // const context = useContext()
 
     const [fav, setFav] = useState([])
+    const [id, setId] = useState()
 
   const getFav = async () => {
     const response = await axios
       .get("http://localhost:5000/api/fav/getfav", {
         headers: {
           "Content-Type": "application/json",
-          "auth-token":
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjQ0MzhmNzQ2ZTUxYjMzNjhjMTE0MDA2In0sImlhdCI6MTY4MjE0OTIzNn0.ZyaYmtgZdU6P9suWE5QkT61KD4jh0SySezl6JZIwG00",
+          "auth-token":`${localStorage.getItem('token')}`,
         },
       })
       .catch((error) => {
         console.log(error.response.data);
       });
 
+    // console.log(response.data[0]._id);
+    setId(response.data[0]._id)
     setFav(response.data[0].favourite)
 
   };
 
   const addFav = async () => {
-    const response = await axios.get(
+    // console.log("token", token);
+    const response = await axios.post(
       "http://localhost:5000/api/fav/addfav",
       {
-        data: {
           favourite: [],
-        },
       },
       {
         headers: {
           "Content-Type": "application/json",
-          "auth-token":
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjQ0MzhmNzQ2ZTUxYjMzNjhjMTE0MDA2In0sImlhdCI6MTY4MjE0OTIzNn0.ZyaYmtgZdU6P9suWE5QkT61KD4jh0SySezl6JZIwG00",
+          "auth-token":`${localStorage.getItem('token')}`,
+        //   "auth-token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjQ0MzhmNzQ2ZTUxYjMzNjhjMTE0MDA2In0sImlhdCI6MTY4MjE0OTIzNn0.ZyaYmtgZdU6P9suWE5QkT61KD4jh0SySezl6JZIwG00",
         },
       }
     );
@@ -45,15 +46,14 @@ const FavState = (props) => {
 
   const updateFav = async (ele) => {
     const response = await axios.put(
-      "http://localhost:5000/api/fav/updatefav/64462440b4c4cfd6f5937e62",
+      `http://localhost:5000/api/fav/updatefav/${id}`,
       {
         favourite: ele,
       },
       {
         headers: {
           "Content-Type": "application/json",
-          "auth-token":
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjQ0MzhmNzQ2ZTUxYjMzNjhjMTE0MDA2In0sImlhdCI6MTY4MjE0OTIzNn0.ZyaYmtgZdU6P9suWE5QkT61KD4jh0SySezl6JZIwG00",
+          "auth-token":`${localStorage.getItem('token')}`,
         },
       }
     );
@@ -61,22 +61,21 @@ const FavState = (props) => {
 
   const removeFav = async (ele) => {
     const response = await axios.put(
-      "http://localhost:5000/api/fav/removefav/64462440b4c4cfd6f5937e62",
+      `http://localhost:5000/api/fav/removefav/${id}`,
       {
         favourite: ele,
       },
       {
         headers: {
           "Content-Type": "application/json",
-          "auth-token":
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjQ0MzhmNzQ2ZTUxYjMzNjhjMTE0MDA2In0sImlhdCI6MTY4MjE0OTIzNn0.ZyaYmtgZdU6P9suWE5QkT61KD4jh0SySezl6JZIwG00",
+          "auth-token":`${localStorage.getItem('token')}`,
         },
       }
     );
   };
 
   return (
-    <favContext.Provider value={{ getFav ,addFav, updateFav, removeFav, fav }}>
+    <favContext.Provider value={{ getFav ,addFav, updateFav, removeFav, fav, id, setId }}>
       {props.children}
     </favContext.Provider>
   );
