46b2206028fa0be4d6cb71895f6a7ae954af8de9
Akash <57212963+Akash-152000@users.noreply.github.com>
2023-04-25 22:23:26 +0530

    Added avatar with dropdown to get fav and integrated logout

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,19 +2,14 @@ import React, { useContext } from "react";
 import "./navbar.css";
 import fetchApiDataContext from "../../context/FetchApiData/fetchApiDataContext";
 import { Link, useNavigate, useLocation } from "react-router-dom";
-import Avatar from '@mui/material/Avatar';
-import { deepOrange, deepPurple } from '@mui/material/colors';
+import Avatar from "@mui/material/Avatar";
+import { deepOrange, deepPurple } from "@mui/material/colors";
 
 const Navbar = () => {
   let location = useLocation();
 
   const context = useContext(fetchApiDataContext);
-  const {
-    searchItem,
-    setSearchItem,
-    search,
-    userName
-  } = context;
+  const { searchItem, setSearchItem, search, userName } = context;
 
   const navigate = useNavigate();
 
@@ -24,10 +19,18 @@ const Navbar = () => {
 
   const handleClick = (e) => {
     e.preventDefault();
-    search(searchItem);
-    setSearchItem("");
-    navigate("/search");
+    if (searchItem === "") {
+    } else {
+      search(searchItem);
+      setSearchItem("");
+      navigate("/search");
+    }
   };
+
+  const handleLogout=()=>{
+    localStorage.removeItem('token')
+    navigate("/login")
+  }
 
   return (
     <div>
@@ -51,7 +54,9 @@ const Navbar = () => {
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <Link
-                  className={`nav-link ${location.pathname==='/'?"text-primary":"text-white"} `}
+                  className={`nav-link ${
+                    location.pathname === "/" ? "text-primary" : "text-white"
+                  } `}
                   aria-current="page"
                   to="/"
                 >
@@ -59,42 +64,107 @@ const Navbar = () => {
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className={`nav-link ${location.pathname==='/movies'?"text-primary":"text-white"}`} to="/movies">
+                <Link
+                  className={`nav-link ${
+                    location.pathname === "/movies"
+                      ? "text-primary"
+                      : "text-white"
+                  }`}
+                  to="/movies"
+                >
                   Movies
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className={`nav-link ${location.pathname==='/tvshows'?"text-primary":"text-white"}`} to="/tvshows">
+                <Link
+                  className={`nav-link ${
+                    location.pathname === "/tvshows"
+                      ? "text-primary"
+                      : "text-white"
+                  }`}
+                  to="/tvshows"
+                >
                   TV Shows
                 </Link>
               </li>
             </ul>
-            <form className="d-flex" role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-                name="search"
-                value={searchItem}
-                id="search"
-                onChange={handleSearch}
-              />
-              <button
-                className="btn btn-outline-light"
-                onClick={handleClick}
-                type="submit"
-              >
-                Search
-              </button>
-              <Link className="btn btn-primary mx-1" to="/login" role="button">
-                Login
-              </Link>
-              <Link className="btn btn-primary mx-1" to="signup" role="button">
-                Signup
-              </Link>
-              <Avatar sx={{ bgcolor: deepOrange[500] }}>{userName.charAt(0).toUpperCase()}</Avatar>
-            </form>
+            {localStorage.getItem("token") ? (
+              <form className="d-flex" role="search">
+                <input
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                  name="search"
+                  value={searchItem}
+                  id="search"
+                  required
+                  onChange={handleSearch}
+                />
+                <button
+                  className="btn btn-outline-light"
+                  onClick={handleClick}
+                  type="submit"
+                >
+                  Search
+                </button>
+
+                <Avatar className="btn btn-secondary "
+                    type="button"
+                    id="dropdownMenuButton2"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  sx={{ bgcolor: deepOrange[500] }}
+                  style={{ cursor: "pointer", marginRight:5, marginLeft:20, border:"none", background:"#ff5722" }}
+                >
+                  {userName.charAt(0).toUpperCase()}
+                </Avatar>
+                <div className="dropdown">
+                  <ul
+                    className="dropdown-menu dropdown-menu-dark"
+                    aria-labelledby="dropdownMenuButton2"
+                    style={{ marginLeft: -155 }}
+                  ><li style={{paddingLeft:15}}>
+                    <p>Signed in as <br /> User1</p>
+                  </li>
+                    <li>
+                      <hr className="dropdown-divider" />
+                    </li>
+                    <li>
+                      <Link className="dropdown-item" to="/favourites">
+                        Your Favourites
+                      </Link>
+                    </li>
+                    <li>
+                      <hr className="dropdown-divider" />
+                    </li>
+                    <li>
+                      <p className="dropdown-item" onClick={handleLogout} >
+                        Logout{" "}
+                        <i className="fa-solid fa-right-from-bracket"></i>
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+              </form>
+            ) : (
+              <>
+                <Link
+                  className="btn btn-primary mx-1"
+                  to="/login"
+                  role="button"
+                >
+                  Login
+                </Link>
+                <Link
+                  className="btn btn-primary mx-1"
+                  to="signup"
+                  role="button"
+                >
+                  Signup
+                </Link>
+              </>
+            )}
           </div>
         </div>
       </nav>
