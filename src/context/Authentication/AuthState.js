b436876e70d9c882a381cd84e5ab72c565a9b2fa
Akash <57212963+Akash-152000@users.noreply.github.com>
2023-04-25 13:24:22 +0530

    Added getuser api call

--- src/context/Authentication/AuthState.js
+++ src/context/Authentication/AuthState.js
@@ -5,14 +5,14 @@ import { useContext } from "react";
 import favContext from "../Favourites/favContext";
 
 const AuthState = (props) => {
-    let navigate = useNavigate();
+  let navigate = useNavigate();
 
-    const context = useContext(favContext)
-    const {addFav} = context
+  const context = useContext(favContext);
+  const { addFav } = context;
 
   const login = async (credentials) => {
-
-      const response = await axios.post(
+    const response = await axios
+      .post(
         "http://localhost:5000/api/auth/login",
         {
           email: credentials.email,
@@ -24,20 +24,20 @@ const AuthState = (props) => {
           },
         }
       )
-      .catch((error)=>{
+      .catch((error) => {
         console.log(error.response.data);
-      })
-      
-      if(response.data.success){
-        localStorage.setItem('token',response.data.authtoken)
-        navigate("/")
-      }
-      return response
-    
+      });
+
+    if (response.data.success) {
+      localStorage.setItem("token", response.data.authtoken);
+      navigate("/");
+    }
+    return response;
   };
 
   const signup = async (credentials) => {
-      const response = await axios.post(
+    const response = await axios
+      .post(
         "http://localhost:5000/api/auth/createuser",
         {
           name: credentials.name,
@@ -50,22 +50,42 @@ const AuthState = (props) => {
           },
         }
       )
-      .catch((error)=>{
+      .catch((error) => {
         console.log(error.response.data);
-      })
-      
-      // console.log(response.data);
-      if(response.data.success){
-        localStorage.setItem('token',response.data.authtoken)
-        addFav()
-        navigate("/")
-      }
-      return response
-    
+      });
+
+    // console.log(response.data);
+    if (response.data.success) {
+      localStorage.setItem("token", response.data.authtoken);
+      addFav();
+      navigate("/");
+    }
+    return response;
+  };
+
+  const getUser = async () => {
+    const response = await axios
+      .get(
+        "http://localhost:5000/api/auth/getuser",
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "auth-token":`${localStorage.getItem('token')}`,
+          },
+        }
+      )
+      .catch((error) => {
+        console.log(error.response.data);
+      });
+
+    if (response) {
+      console.log(response);
+    }
+    return response;
   };
 
   return (
-    <authContext.Provider value={{ login, signup }}>
+    <authContext.Provider value={{ login, signup, getUser }}>
       {props.children}
     </authContext.Provider>
   );
