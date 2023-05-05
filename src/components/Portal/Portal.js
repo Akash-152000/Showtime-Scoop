ed0c59d41abf144c0208559b6577a5dade799321
Akash <57212963+Akash-152000@users.noreply.github.com>
2023-05-05 12:53:30 +0530

    Made it somewhat responsive

--- src/components/Portal/Portal.js
+++ src/components/Portal/Portal.js
@@ -6,21 +6,28 @@ import PortalDesc from "./Portalcomponents/PortalDesc";
 import favContext from "../../context/Favourites/favContext";
 import { useNavigate } from "react-router-dom";
 
-
 const Portal = () => {
   let navigate = useNavigate();
 
   const context = useContext(fetchApiDataContext);
-  const { title, desc, releaseDate, rating, setShowPortal, poster, eleInfo,setFavUpdated,
-    favUpdated } =  context;
+  const {
+    title,
+    desc,
+    releaseDate,
+    rating,
+    setShowPortal,
+    poster,
+    eleInfo,
+    setFavUpdated,
+    favUpdated,
+  } = context;
 
-  const contextFav = useContext(favContext)
-  const {getFav,updateFav, removeFav, fav} = contextFav
+  const contextFav = useContext(favContext);
+  const { getFav, updateFav, removeFav, fav } = contextFav;
 
   const closePortal = () => {
     setShowPortal(false);
   };
-
 
   document.onkeydown = function (evt) {
     if (evt.key === "Escape") {
@@ -32,47 +39,67 @@ const Portal = () => {
     // console.log(fav,ele,"ele ");
     console.log(fav.includes(ele));
     if (fav.includes(ele)) {
-      removeFav(ele)
-      setFavUpdated(!favUpdated)
-      
+      removeFav(ele);
+      setFavUpdated(!favUpdated);
     } else {
-      updateFav(ele)
-      setFavUpdated(!favUpdated)
+      updateFav(ele);
+      setFavUpdated(!favUpdated);
     }
     console.log(fav, eleInfo);
   };
 
-  useEffect(()=>{
-    if(localStorage.getItem('token')){
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
       getFav();
+    } else {
+      navigate("/login");
     }
-    else{
-      navigate("/login")
-    }
-  },[favUpdated])
-
+  }, [favUpdated]);
 
   return ReactDOM.createPortal(
     <>
       <div className="modal-wrapper text-light" onClick={closePortal}></div>
-      <div className="portal text-white d-flex pe-3">
+      <div
+        className={
+          window.innerWidth <= 600
+            ? "portal text-white text-center"
+            : "portal text-white d-flex pe-3"
+        }
+      >
         <img
           src={`https://image.tmdb.org/t/p/w500${poster}`}
           className="portal-img-top"
           alt="..."
         />
 
-        <div className="content text-center m-2">
-          <h3 className="title text-center">{title}</h3>
-          <h6 className="description px-3 pt-3">{desc}</h6>
-          <div className="date-rate d-flex justify-content-between px-5 pt-2">
-            <h5 className="releaseDate">Release Date: {releaseDate}</h5>
-            <h5 className="rating">Rating: {Math.round(rating*10)/10}</h5>
+        {window.innerWidth <= 600 ? (
+          <div className="content text-center m-2">
+            <h5 className="title text-center">{title}</h5>
+            <h6 className="description px-3 pt-3">{desc}</h6>
+            <div className="date-rate d-flex justify-content-between px-5 pt-2">
+              <h5 className="releaseDate">{releaseDate}</h5>
+              <h5 className="rating">Rating: {Math.round(rating * 10) / 10}</h5>
+            </div>
+            <PortalDesc />
           </div>
-          <PortalDesc />
-        <i className={`${fav.includes(eleInfo)?"fa-solid":"fa-regular"} fa-heart fa-2xl heart` } style={{marginTop:40,cursor:"pointer"}} onClick={()=>handleFav(eleInfo)}></i>
-        </div>
-        <h5 onClick={closePortal} className="close" >X</h5>
+        ) : (
+          <>
+            <div className="content text-center m-2">
+              <h3 className="title text-center">{title}</h3>
+              <h6 className="description px-3 pt-3">{desc}</h6>
+              <div className="date-rate d-flex justify-content-between px-5 pt-2">
+                <h5 className="releaseDate">Release Date: {releaseDate}</h5>
+                <h5 className="rating">
+                  Rating: {Math.round(rating * 10) / 10}
+                </h5>
+              </div>
+              <PortalDesc />
+            </div>
+            <h5 onClick={closePortal} className="close">
+              X
+            </h5>
+          </>
+        )}
       </div>
     </>,
     document.getElementById("portal")
--- src/screens/Favourites/Favourites.js
+++ src/screens/Favourites/Favourites.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Favourites = () => {
   return (
-    <div>Favourites</div>
+    <div >
+      <h1 className="text-center text-white mt-5">Coming soon</h1>
+    </div>
   )
 }
 
